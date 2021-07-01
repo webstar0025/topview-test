396c58bc2f003a8d5dd5494ff7974c33292a8d5e
ben <superman.star25@gmail.com>
2021-07-01 00:10:32 -0400

    implement UI and functions of header controllers

---
+++ topview-home-exercise/src/components/Header/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+
+import {ReactComponent as ArrowIcon } from '../../assets/ico-arrow.svg';
+import {ReactComponent as CloseIcon } from '../../assets/ico-circle-close.svg';
+import {ReactComponent as PlusIcon } from '../../assets/ico-circle-plus.svg';
+import FinalStateIcon from '../../assets/ico-final-state.png';
+import LocationIcon from '../../assets/ico-location.png';
+import ExchangeIcon from '../../assets/ico-exchange.png';
+
+import styles from './Header.module.css';
+
+const FILTERS = ['Bus Stops', 'Attractions', 'Parks', 'Museums'];
+
+export const Header = () => {
+  const [locations, setLocations] = useState([1, 0]);
+
+  const addDestination = () => {
+    if (locations.length > 2) return;
+    setLocations([...locations, 0]);
+  }
+
+  const removeLocation = () => {
+    setLocations([1, 0]);
+  }
+
+  const exchangeLocation = () => {
+    const newLocations = [...locations].reverse();
+    setLocations([...newLocations]);
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.locationSection}>
+        <ArrowIcon className={styles.arrowIcon} />
+        <div className={styles.locaitonsContainer}>
+          <div className={styles.locations}>
+            {locations.length === 2 && (
+              <img
+                src={ExchangeIcon}
+                alt=""
+                className={styles.exchangeIcon}
+                onClick={exchangeLocation}
+              />
+            )}
+            {locations.map((location, index) => 
+              <div className={styles.locationContent}>
+                <div className={styles.locationLogo}>
+                  {(locations.length < 3 || index === 0) ? (
+                    <img
+                      width={20}
+                      height={20}
+                      src={index === 0 ? FinalStateIcon : LocationIcon}
+                      alt=""
+                    />
+                  ) : (
+                    <div className={styles.locationAddress}>
+                      <span>{index === 1 ? 'A' : 'B'}</span>
+                    </div>
+                  )}
+                </div>
+                {location ? (
+                  <input
+                    className={styles.location}
+                    value="Current Location"
+                    readOnly
+                  />
+                ) : (
+                  <input
+                    className={styles.location}
+                    value=""
+                    placeholder="Choose Destination"
+                    readOnly
+                  />
+                )}
+                {(locations.length > 2 && index > 0) && (
+                  <CloseIcon
+                    className={styles.closeIcon}
+                    onClick={removeLocation}
+                  />
+                )}
+              </div>
+            )}
+          </div>
+          <div className={styles.locationContent}>
+            <PlusIcon className={styles.plusIcon} />
+            <span
+              className={styles.addButton}
+              onClick={addDestination}
+            >
+              Add destination
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.filterContainer}>
+        {FILTERS.map((item, index) =>
+          <div
+            key={index} 
+            className={styles.filterButton}
+          >
+            {item}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
